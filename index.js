2adf8204cc736d1f7d6f41fde9a948c234e7a812
JosephBorrelli <josephborrelli80@gmail.com>
2022-12-27 17:13:39 -0500

    commit tree 344ff8a755fea89b34da59ced1c1bab7ddf93f23

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( namesArray ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful surprise gift!` )
+    }  
+    return thankYouCards
+    }
+    writeCards(["Charlie", "Samip", "Ali","Guadalupe", "Ollie", "Aki"])
+    console.log(writeCards(["Charlie", "Samip", "Ali", "Guadalupe", "Ollie", "Aki"]))
+
+
+function countDown (){
+    let number = 10
+    while (number > -1){
+    console.log (number--)}}
